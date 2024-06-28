d7017f85f1de6822dc4719293dd5b215725db4b0
Lars Nieuwenhuizen <l.nieuwenhuizen@fullstaq.com>
2024-06-28 10:01:21 +0000

    fix: Dev backendservers did not actually start

--- backendServers.go
+++ backendServers.go
@@ -5,6 +5,7 @@ package loadbalancer
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 // startBackendServers starts the backend servers in separate goroutines
@@ -14,15 +15,23 @@ func startBackendServers() {
 		return
 	}
 	for _, server := range Configuration.BackendServers {
-		go startServer(server)
+		serverParts := strings.Split(server, ":")
+		if len(serverParts) != 3 {
+			log.Fatalf("invalid server address: %s", serverParts)
+			return
+		}
+		port := serverParts[2]
+		log.Println(":" + port)
+		go startServer(":" + port)
 	}
 }
 
 // startServer starts a dummy backend server
-func startServer(server string) {
-	log.Println("Starting backend server", server)
+func startServer(serverPort string) {
+	log.Println("Starting backend server", serverPort)
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Backend server %s received request", server)
+		log.Printf("Backend server %s received request", serverPort)
+		w.Write([]byte("Hello from backend server " + serverPort))
 	})
-	http.ListenAndServe(server, handler)
+	http.ListenAndServe(serverPort, handler)
 }
