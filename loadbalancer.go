640fd3fa867f3a531219ac6e6f0a578c2061db5a
Lars Nieuwenhuizen <l.nieuwenhuizen@fullstaq.com>
2024-06-28 10:01:59 +0000

    feat: Process the backend server request and pass it to the client

--- loadbalancer.go
+++ loadbalancer.go
@@ -2,6 +2,7 @@ package loadbalancer
 
 import (
 	"flag"
+	"io"
 	"log"
 	"net/http"
 )
@@ -33,14 +34,59 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 		"Proxying request to backend server",
 		Configuration.BackendServers[chosenServerIndex],
 	)
-	http.Redirect(
-		w,
-		r,
-		Configuration.BackendServers[chosenServerIndex],
-		http.StatusTemporaryRedirect,
-	)
+
+	err := processRequestFromBackend(w, r)
+	if err != nil {
+		log.Fatal("Error processing request from backend:", err)
+	}
 
 	decideNextServerIndex()
+}
+
+// processRequestFromBackend sends the request to the backend server so we can return the actual response to the client
+func processRequestFromBackend(w http.ResponseWriter, r *http.Request) error {
+	client := &http.Client{}
+	backendServer := Configuration.BackendServers[chosenServerIndex]
+
+	// Create a new request to the backend server
+	backendRequest, err := http.NewRequest(r.Method, backendServer, r.Body)
+	if err != nil {
+		log.Println("Error creating request to backend server:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return err
+	}
+
+	// Copy headers from the original request to the backend request
+	backendRequest.Header = r.Header
+
+	// Send the request to the backend server
+	backendResponse, err := client.Do(backendRequest)
+	if err != nil {
+		log.Println("Error sending request to backend server:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return err
+	}
+	defer backendResponse.Body.Close()
+
+	// Copy the response headers from the backend response to the client response
+	for key, values := range backendResponse.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+
+	// Set the status code of the client response to the status code of the backend response
+	w.WriteHeader(backendResponse.StatusCode)
+
+	// Copy the response body from the backend response to the client response
+	_, err = io.Copy(w, backendResponse.Body)
+	if err != nil {
+		log.Println("Error copying response body:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return err
+	}
+
+	return nil
 }
 
 func decideNextServerIndex() {
