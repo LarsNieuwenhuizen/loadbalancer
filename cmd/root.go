694a72a7b66f2080fa9cccdfdc80f87a18e67614
Lars Nieuwenhuizen <l.nieuwenhuizen@fullstaq.com>
2024-07-24 18:50:03 +0000

    feat: Implement cobra

---
+++ cmd/root.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/LarsNieuwenhuizen/loadbalancer"
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "loadbalancer",
+	Short: "A loadblancer written in go",
+	Long:  `This loadbalancer passes requests to configured backend servers.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		configFlag, _ := cmd.Flags().GetString("config")
+		_, err := os.Stat("./main/config.yaml")
+		if errors.Is(err, fs.ErrNotExist) {
+			fmt.Println("Config file " + configFlag + " does not exist, check the path or change it with the --config flag")
+			os.Exit(1)
+		}
+
+		lb := loadbalancer.LoadBalancer{}
+		lb.ConfigureFromYaml(configFlag)
+		fmt.Println("Loadbalancer started, configured with", len(lb.Configuration.BackendServers), "backend servers")
+		lb.Start()
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	version := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of loadbalancer",
+		Long:  `All software has versions. This is loadbalancer's`,
+		Run: func(cmd *cobra.Command, args []string) {
+			versionFile, err := os.ReadFile("VERSION")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(versionFile))
+		},
+	}
+	rootCmd.AddCommand(version)
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	homeDir, _ := os.UserHomeDir()
+	cfgFile := filepath.Join(homeDir, ".loadbalancer.yaml")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", cfgFile, "config yaml file")
+}
